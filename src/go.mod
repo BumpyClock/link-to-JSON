150fcf6afb2f27c9c4a2b01fa2818420e112e0d7
Aditya Sharma <adityaksharma@gmail.com>
2024-04-19 09:03:13 -0700

    Update PORT environment variable in .env to use port 3030 instead of 8080

--- src/go.mod
+++ src/go.mod
@@ -3,18 +3,26 @@ module link-to-json
 go 1.22.1
 
 require (
+	github.com/BumpyClock/go-link2json v0.0.5
+	github.com/gin-contrib/cors v1.7.1
+	github.com/gin-gonic/gin v1.9.1
+	github.com/joho/godotenv v1.5.1
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/time v0.5.0
+)
+
+require (
 	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/antchfx/htmlquery v1.3.1 // indirect
 	github.com/antchfx/xmlquery v1.4.0 // indirect
 	github.com/antchfx/xpath v1.3.0 // indirect
-	github.com/bytedance/sonic v1.11.3 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
-	github.com/chenzhuoyu/iasm v0.9.1 // indirect
+	github.com/bytedance/sonic v1.11.5 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.3 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/cors v1.7.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
@@ -22,28 +30,26 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	github.com/pelletier/go-toml/v2 v2.2.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- src/link-to-JSON.go
+++ src/link-to-JSON.go
@@ -5,137 +5,20 @@ import (
 	"net/http"
 	URL "net/url"
 	"os"
-	"strconv"
 	"time"
 
+	link2json "github.com/BumpyClock/go-link2json"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/gocolly/colly"
-	"github.com/joho/godotenv"
-	"github.com/patrickmn/go-cache" // Caching package
+	"github.com/joho/godotenv" // Caching package
 	"github.com/sirupsen/logrus"
 	"golang.org/x/time/rate" // Rate limiter
 )
 
-type ResponseItem struct {
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Images      []WebImage `json:"images"`
-	Sitename    string     `json:"sitename"`
-	Favicon     string     `json:"favicon"`
-	Duration    int        `json:"duration"`
-	Domain      string     `json:"domain"`
-	URL         string     `json:"url"`
-}
-
-type WebImage struct {
-	URL    string `json:"url"`
-	Alt    string `json:"alt,omitempty"`
-	Type   string `json:"type,omitempty"`
-	Width  int    `json:"width,omitempty"`
-	Height int    `json:"height,omitempty"`
-}
-
 var (
 	rateLimiter = rate.NewLimiter(1, 3) // Allows 1 request per second with a burst capacity of 3
-	cch         = cache.New(30*time.Minute, 60*time.Minute)
-	userAgent   string
+
 )
-
-func fetchMetadata(url string) (*ResponseItem, error) {
-
-	// Check cache first
-	if cached, found := cch.Get(url); found {
-		return cached.(*ResponseItem), nil
-	}
-
-	c := colly.NewCollector()
-	c.OnRequest(func(r *colly.Request) {
-		logrus.Info("Visiting", r.URL)
-		r.Headers.Set("User-Agent", userAgent)
-	})
-	result := &ResponseItem{URL: url, Images: []WebImage{}}
-	result.Domain = getBaseDomain(url)
-	webImage := WebImage{}
-
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		if result.Title == "" {
-			result.Title = e.Text
-		}
-	})
-	c.OnHTML(`meta[name="description"]`, func(e *colly.HTMLElement) {
-		result.Description = e.Attr("content")
-	})
-	c.OnHTML(`link[rel="icon"], link[rel="shortcut icon"], link[rel="apple-touch-icon"], link[rel="apple-touch-icon-precomposed"]`, func(e *colly.HTMLElement) {
-		if result.Favicon == "" {
-			result.Favicon = result.Domain + e.Attr("href")
-		}
-	})
-	c.OnHTML(`meta[property="og:site_name"]`, func(e *colly.HTMLElement) {
-		result.Sitename = e.Attr("content")
-	})
-	c.OnHTML(`meta[property="og:image"]`, func(e *colly.HTMLElement) {
-		webImage.URL = e.Attr("content")
-	})
-	c.OnHTML(`meta[property="og:image:alt"]`, func(e *colly.HTMLElement) {
-		webImage.Alt = e.Attr("content")
-	})
-	c.OnHTML(`meta[property="og:image:type"]`, func(e *colly.HTMLElement) {
-		webImage.Type = e.Attr("content")
-	})
-	c.OnHTML(`meta[property="og:image:width"]`, func(e *colly.HTMLElement) {
-		width, err := strconv.Atoi(e.Attr("content"))
-		if err == nil {
-			webImage.Width = width
-		}
-	})
-	c.OnHTML(`meta[property="og:image:height"]`, func(e *colly.HTMLElement) {
-		height, err := strconv.Atoi(e.Attr("content"))
-		if err == nil {
-			webImage.Height = height
-		}
-	})
-	c.OnScraped(func(r *colly.Response) {
-		result.Images = append(result.Images, webImage)
-		logrus.Info("Scraping finished", url)
-	})
-
-	// Handle visiting the URL
-	err := c.Visit(url)
-	if err != nil {
-		logrus.Error("Failed to visit URL: ", err)
-		return nil, err
-	}
-
-	if result.Sitename == "" {
-		c2 := colly.NewCollector()
-		logrus.Info("Visiting", result.Domain)
-		c2.OnHTML(`meta[property="og:title"]`, func(e *colly.HTMLElement) {
-			result.Sitename = e.Attr("content")
-
-		})
-
-		err = c2.Visit(result.Domain)
-		if err != nil {
-			logrus.Error("Failed to visit base domain: ", err)
-			return nil, err
-		}
-	}
-
-	// Cache the result
-	cch.Set(url, result, cache.DefaultExpiration)
-
-	return result, nil
-}
-
-func getBaseDomain(url string) string {
-	parsedURL, err := URL.Parse(url)
-	if err != nil {
-		return ""
-	}
-
-	return parsedURL.Scheme + "://" + parsedURL.Host
-}
 
 func main() {
 	router := gin.Default()
@@ -185,7 +68,7 @@ func main() {
 			return
 		}
 
-		metadata, err := fetchMetadata(url)
+		metadata, err := link2json.GetMetadata(url)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch metadata"})
 			return
@@ -198,4 +81,6 @@ func main() {
 	})
 
 	router.Run(":" + port)
+	logrus.Info("Server started on port: ", port)
+
 }
